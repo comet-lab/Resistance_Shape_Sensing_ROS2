5d00102ea08f30158986d38e26c99fa5f9d358d8
Wenpeng Wang <wwang11@wpi.edu>
2023-08-15 11:13:26 -0400

    init ROS compilable debug

--- cdm_tip_publisher/cdm_tip_publisher/publisher_member_function.py
+++ cdm_tip_publisher/cdm_tip_publisher/publisher_member_function.py
@@ -17,13 +17,112 @@ import cv2
 import pyrealsense2 as rs
 import serial
 import csv
-from usb_rs import Usb_rs
+# from usb_rs import Usb_rs
 import threading
+import time
+import tkinter.messagebox
 
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 from cdm_tip_msgs.msg import Resistance
+
+
+class Usb_rs:
+
+    def __init__(self, gui=False):
+        self.ser = serial
+        self.gui = gui
+    
+    #Open port
+    def open(self, port, speed):
+        ret = False
+
+        try:
+            self.ser = serial.Serial(port, speed,timeout=0)
+            ret = True
+        except Exception as e:
+            if self.gui == True:
+                tkinter.messagebox.showerror("Open Error", e)
+            else:
+                print("Open error")
+                print(e)
+        
+        return ret
+
+    #Close port
+    def close(self):
+        ret = False
+
+        try:   
+            self.ser.close()
+            ret = True
+        except Exception as e:
+            if self.gui == True:
+                tkinter.messagebox.showerror("Close Error", e)
+            else:
+                print("Close error")
+                print(e)
+        
+        return ret
+    #Send command
+    def sendMsg(self, strMsg):
+        ret = False
+
+        try:
+            strMsg = strMsg + '\r\n'                #Add a terminator, CR+LF, to transmitted command
+            self.ser.write(bytes(strMsg, 'utf-8'))  #Convert to byte type and send
+            ret = True
+        except Exception as e:
+            if self.gui == True:
+                tkinter.messagebox.showerror("Send Error", e)
+            else:
+                print("Send Error")
+                print(e)
+
+        return ret
+    
+    #Receive
+    def receiveMsg(self, timeout):
+
+        msgBuf = bytes(range(0))                    #Received Data
+
+        try:
+            start = time.time()                     #Record time for timeout
+            while True:
+                if self.ser.inWaiting() > 0:        #Is exist the data in the receive buffer?
+                    rcv = self.ser.read(1)          #Receive 1 byte
+                    if rcv == b"\n":                #End the loop when LF is received
+                        msgBuf = msgBuf.decode('utf-8')
+                        break
+                    elif rcv == b"\r":              #Ignore the terminator CR
+                        pass
+                    else:
+                        msgBuf = msgBuf + rcv
+                
+                #Timeout processing
+                if  time.time() - start > timeout:
+                    msgBuf = "Timeout Error"
+                    break
+        except Exception as e:
+            if self.gui == True:
+                tkinter.messagebox.showerror("Receive Error", e)
+            else:
+                print("Receive Error")
+                print(e)
+            msgBuf = "Error"
+
+        return msgBuf
+    
+    #Transmit and receive commands
+    def SendQueryMsg(self, strMsg, timeout):
+        ret = Usb_rs.sendMsg(self, strMsg)
+        if ret:
+            msgBuf_str = Usb_rs.receiveMsg(self, timeout)   #Receive response when command transmission is succeeded
+        else:
+            msgBuf_str = "Error"
+
+        return msgBuf_str
 
 #Timeout(1sec)
 Timeout_default = 1
@@ -31,14 +130,14 @@ Timeout_default = 1
 ## Resistance Reading Function
 def read_R():
     #Instantiation of the usb_rs communication class
-    serial = Usb_rs()
+    serial1 = Usb_rs()
 
     #Connect
     # print("Port?")
     port = "COM3"  
     # print("Speed?")
     speed = 9600
-    if not serial.open(port,speed):
+    if not serial1.open(port,speed):
         return
     
     #Send and receive commands
@@ -50,13 +149,13 @@ def read_R():
     #     break
         #If the command contains "?"
     if "?" in command :
-        msgBuf = serial.SendQueryMsg(command, Timeout_default)
+        msgBuf = serial1.SendQueryMsg(command, Timeout_default)
         #print(msgBuf) 
         #Send only
     else:
-        serial.sendMsg(command)
+        serial1.sendMsg(command)
         
-    serial.close()
+    serial1.close()
     return msgBuf
 
 ## Color Filter Tool Functions
